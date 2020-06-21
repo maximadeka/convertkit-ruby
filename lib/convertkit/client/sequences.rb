d004f4ff59d7dfaeb78e19562e9acf08933494c2
Eric Alli <hello@ericalli.com>
2020-06-21 16:27:23 -0700

    Add sequence subscriptions endpoint

--- lib/convertkit/client/sequences.rb
+++ lib/convertkit/client/sequences.rb
@@ -5,6 +5,10 @@ module Convertkit
         connection.get("sequences")
       end
 
+      def sequence_subscriptions(sequence_id, options = {})
+        connection.get("sequences/#{sequence_id}/subscriptions", options)
+      end      
+      
       def add_subscriber_to_sequence(sequence_id, email, options = {})
         connection.post("sequences/#{sequence_id}/subscribe") do |f|
           f.params['email'] = email
